88052682068f86c697b29326981999e3ed79bbf9
Matthew Butterfield <buttermatt@ecs.vuw.ac.nz>
2020-08-05 12:47:45 +1200

    add navigation buttons to life total page

--- src/pages/LifeCounterNewGame/LifeCounterNewGame.tsx
+++ src/pages/LifeCounterNewGame/LifeCounterNewGame.tsx
@@ -13,6 +13,36 @@ const LifeCounterNewGame: React.FC = () => {
       {/* Displays the Header */}
       <Header headerLabel="Life Counter - New Game"/>
 
+      {/* <IonItem lines="full">
+        <IonItem class="test" lines="none" color="secondary">
+          <IonTitle class="buttontext">Number<br/>of players</IonTitle>
+        </IonItem>
+        <IonItem class="test" lines="none" color="warning">
+          <IonTitle class="buttontext">Starting<br/>Life Totals</IonTitle>
+        </IonItem>
+        <IonItem class="test" href='../quick-search' lines="none" color="primary">
+          <IonTitle class="buttontext">Start Game!</IonTitle>
+        </IonItem>
+      </IonItem> */}
+
+    <IonRow>
+      <IonCol>
+        <IonButton class="lifeNavigationButtons" color="warning" expand="full">
+          Number of<br/> Players
+        </IonButton>
+      </IonCol>
+      <IonCol>
+        <IonButton class="lifeNavigationButtons" color="success" expand="full">
+          Life Totals
+        </IonButton>
+      </IonCol>
+      <IonCol>
+        <IonButton class="lifeNavigationButtons" color="tertiary" expand="full">
+          Confirmation
+        </IonButton>
+      </IonCol>
+    </IonRow>
+
       <IonContent>
         <IonGrid>
           <IonRow>
