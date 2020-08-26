1b2de1ff3f5892afef8f684453c54eb3d138072d
Matthew Butterfield <buttermatt@ecs.vuw.ac.nz>
2020-08-26 16:07:11 +1200

    Create dice page * Still requries history to be completed

--- src/pages/Dice/Dice.tsx
+++ src/pages/Dice/Dice.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonPage, IonGrid, IonRow, IonCol, IonButton, IonInput, IonLabel, IonList, IonCard } from '@ionic/react';
 import './Dice.css';
 import FooterTabs from '../../components/FooterTabs/FooterTabs';
 import Header from '../../components/Header/Header';
-import WorkInProgress from '../../components/WorkInProgress/WorkInProgress';
+import { rollCustom, flipCoin, rollD6, rollD20 } from './DiceHelper';
 
 const Dice: React.FC = () => {
+  const [die,setDie] = useState('Roll a Dice');
+  const [dieType,setDieType] = useState('');
+  const [custom,setCustom] = useState(1);
   return (
     
     <IonPage>
@@ -15,7 +18,92 @@ const Dice: React.FC = () => {
 
       <IonContent>
 
-        <WorkInProgress name={"Dice"}/>
+        <IonList>
+          <IonCard>
+            <IonLabel class="diceOutputText">
+              {die}
+            </IonLabel>
+            <IonLabel class="diceOutputSubText">
+              {dieType}
+            </IonLabel>
+          </IonCard>
+
+          <IonCard>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  <IonButton class="diceButton"
+                    onClick={e => 
+                      (
+                        setDie(flipCoin()),
+                        setDieType('Coin')
+                      )
+                  }>
+                    <IonLabel class="diceButtonText">
+                      Coin
+                    </IonLabel>
+                  </IonButton>
+                </IonCol>
+                <IonCol>
+                  <IonButton class="diceButton"
+                    onClick={e =>
+                      (
+                        setDie(rollD6().toString()),
+                        setDieType('6 Sided Die')
+                      )
+                  }>
+                    <IonLabel class="diceButtonText">
+                      D6
+                    </IonLabel>
+                  </IonButton>
+                </IonCol>
+                <IonCol>
+                  <IonButton class="diceButton"
+                    onClick={e =>
+                      (
+                        setDie(rollD20().toString()),
+                        setDieType('20 Sided Die')
+                      )
+                  }>
+                    <IonLabel class="diceButtonText">
+                      D20
+                    </IonLabel>
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <IonInput
+                    placeholder="Custom" type="number"
+                    onIonChange={e => {
+                        setCustom(Math.ceil(Number(e.detail.value!)))
+                    }}/>
+                </IonCol>
+                <IonCol>
+                  <IonButton
+                  onClick={e => {
+                    var customDie = rollCustom(custom);
+                    if(customDie < 1){
+                      setDie('Please use a positive integer');
+                      setDieType('Custom value given: '+ custom);
+                    } else {
+                      setDie(customDie.toString())
+                      setDieType('Custom ('+ custom +' Sided) Die')
+                    }
+                  }}>
+                    custom
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonCard>
+
+          <IonCard>
+            <IonLabel>
+              History:
+            </IonLabel>
+          </IonCard>
+        </IonList>
 
       </IonContent>
 
---
+++ src/states/DiceHistoryState.tsx
@@ -0,0 +1,79 @@
+////////////////////////
+/*Imports*/
+////////////////////////
+
+import { Plugins } from '@capacitor/core';
+
+////////////////////////
+/*Local Initialisation*/
+////////////////////////
+
+const { Storage } = Plugins;
+
+const historyStorageKey   : string = "diceHistory";   // String that dictates the string that the history of dice rolls is stored under in capacitor.
+const maxHistoryCount     : number = 20;
+
+////////////////////////
+/*DiceHistoryState*/
+////////////////////////
+
+/**
+ * DiceHistoryState Interface, used to store information about a single roll.
+ */
+export interface DiceHistoryState {
+  dieType:    string
+  dieValue:   string
+}
+
+////////////////////////
+/*Capacitor Storage for History Storage*/
+////////////////////////
+
+/**
+ * Saves an single of DiceHistoryState into storage.
+ * This is used for DiceHistory to show what has been previously been rolled up.
+ * This will retrieve the previous entries, append to it, and remove the earliest record if it goes over the max.
+ * @param currentDiceHistoryState - the DiceHistoryState to be saved in storage.
+ */
+export async function saveDiceHistory(currentDiceHistoryState : DiceHistoryState) : Promise<boolean> {
+
+  /*Get the Current Dice History and make sure it meets Maximum Counts*/
+  let diceHistory : DiceHistoryState[] = await getDiceHistory();
+  while (diceHistory.length >= maxHistoryCount) {
+    diceHistory.shift();
+  }
+  
+  /*Add New Dice URL Link*/
+  diceHistory.push(currentDiceHistoryState);
+
+  /*JSONStringify the Updated Dice History Array*/
+  let valueToSave : string = JSON.stringify(diceHistory);
+
+  /*Save the Dice History into Capacitor Storage*/
+  const returnValue = await Storage.set({
+    key: historyStorageKey,
+    value: valueToSave
+  }).then( () => {
+    return true;
+  }).catch(err => {
+    console.log(err);
+    return false;
+  });
+
+  return returnValue;
+}
+
+/**
+ * Retrieves the DiceHistoryState Array in storage.
+ * This is used for DiceHistory to retrieve the previously saved data.
+ */
+export async function getDiceHistory() : Promise<DiceHistoryState[]> {
+
+  const storageReturn = await Storage.get({key: historyStorageKey});
+
+  if (typeof storageReturn.value === 'string') {
+    return (JSON.parse(storageReturn.value) as DiceHistoryState[]);
+  } else { //Null Case
+    return [];
+  }
+}
